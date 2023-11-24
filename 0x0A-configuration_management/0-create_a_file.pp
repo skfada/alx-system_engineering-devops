385bdf4794ebfd4e828d6c9cc3ed1ea27795786a
skfada <adamusolomon44@gmail.com>
2023-11-24 14:01:57 +0100

    creating a file in tmp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# to create a file in /tmp
+
+file { '/tmp/school':
+  ensure  => file,
+  content => "I love Puppet\n",
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
---
+++ 0x0A-configuration_management/README.md
@@ -0,0 +1,4 @@
+#### PROJECT ASSIGNMENT ON 0x0A. Configuration management
+
+- Tasks
+- 0. Create a file
