b1cffc98c04063e44f45e63e5643b645ff9a55a1
skfada <adamusolomon44@gmail.com>
2023-11-25 08:59:23 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,25 +1,8 @@
 # to Use Puppet to install flask from pip3
 
-package { 'python3-pip':
-  ensure => installed,
-}
-
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
-}
-
-file { '/usr/local/bin/check_flask_version':
-  ensure => file,
-  content => "#!/bin/bash\n/usr/bin/flask --version",
-  mode    => '0755',
-  require => Exec['install_flask'],
-}
-
-exec { 'run_check_flask_version':
-  command     => '/usr/local/bin/check_flask_version',
-  refreshonly => true,
-  subscribe   => File['/usr/local/bin/check_flask_version'],
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/usr/local/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep Version | grep -q 2.1.0',
 }
 
