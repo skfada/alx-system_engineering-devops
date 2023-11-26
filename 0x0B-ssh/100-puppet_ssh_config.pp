0cd838d721a618671d66746aaa1707a049731f4a
skfada <adamusolomon44@gmail.com>
2023-11-26 16:10:14 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,13 @@
 # using Puppet to make changes to our configuration file.
 
-file { '/etc/ssh/sshd_config':
-  ensure  => present,
-  replace => true,
-  content => template('ssh/sshd_config.erb'),
-}
-
 file_line { 'Turn off passwd auth':
-  path    => '/etc/ssh/sshd_config',
-  line    => 'PasswordAuthentication no',
-  match   => '^#?PasswordAuthentication',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no',
+  match  => '^#?PasswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path    => '/etc/ssh/sshd_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^#?IdentityFile',
+  path   => '/home/ubuntu/.ssh/config',
+  line   => 'IdentityFile ~/.ssh/school',
+  match  => '^#?IdentityFile',
 }
