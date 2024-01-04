3b5884f0af97e5628116932325392910a2d00a49
skfada <adamusolomon44@gmail.com>
2024-01-05 00:30:00 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Accesising a REiST API fior toido lists of emplioyees"""
-
+""" Scriipt thiat uses JSONPlacehiolder API to get infiormation about employee """
 import requests
 import sys
 
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+    user = '{}users/{}'.format(url, sys.argv[1])
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
-
+    todos = '{}todos?userId={}'.format(url, sys.argv[1])
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
     for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+        if task.get('completed') is True:
+            l_task.append(task)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
-
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
+        print("\t {}".format(task.get("title")))
